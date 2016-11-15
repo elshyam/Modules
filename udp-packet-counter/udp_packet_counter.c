0cd762bad932beed077cc07c5f28e17dd0e79e6b
shyam saini <mayhs11saini@gmail.com>
2016-11-15 20:17:46 +0530

    use printk() in place of pr_debug()

--- udp-packet-counter/udp_packet_counter.c
+++ udp-packet-counter/udp_packet_counter.c
@@ -4,12 +4,12 @@
 #include <linux/ip.h>
 #include <linux/in.h>
 
-
 static struct nf_hook_ops nfho;
 int count = 0;
 
-static unsigned int UDP_counter_hook_func(const struct nf_hook_ops *ops, struct sk_buff *skb, const struct net_device *in,
-		const struct net_device *out, int (*okfn)(struct sk_buff *))
+static unsigned int UDP_counter_hook_func(const struct nf_hook_ops *ops, 
+					  struct sk_buff *skb, const struct net_device *in,
+		                          const struct net_device *out, int (*okfn)(struct sk_buff *))
 {
 	struct iphdr *ipheader;
 	
@@ -18,8 +18,7 @@ static unsigned int UDP_counter_hook_func(const struct nf_hook_ops *ops, struct 
 
 	ipheader = ip_hdr(skb);   /*Extracts IP header from sk_buff*/
 	
-
-	/*check for UDP packet comparing ip header protocol */
+	/*check for UDP packet comparing ip header nnotocol */
 	if(ipheader->protocol ==  IPPROTO_UDP){       /* see full list of protocols in <linux/in.h> */
 		count = count + 1;
 		printk(KERN_INFO "UDP Paket number %d\n", count);
@@ -33,7 +32,7 @@ static unsigned int UDP_counter_hook_func(const struct nf_hook_ops *ops, struct 
 static int __init udpCounter_init(void)
 {
 	int res;
-
+	
 	nfho.hook = (nf_hookfn *)UDP_counter_hook_func;    /* hook function */
 	nfho.hooknum = NF_INET_POST_ROUTING;         /* received packets */
 	nfho.pf = PF_INET;                          /* IPv4 */
@@ -42,24 +41,22 @@ static int __init udpCounter_init(void)
 	res = nf_register_hook(&nfho);              /*register netfilter hook structure */
 
 	if (res < 0) {
-		pr_err("udp _Packet_counter: error in nf_register_hook()\n");
+		printk(KERN_INFO "udp _Packet_counter: error in nf_register_hook()\n");
 		return res;
 	}
-
-	pr_debug("udp_packet_counter loaded\n");
-
+	printk(KERN_INFO "udp_packet_counter loaded\n");
+	
 	return 0;
 }
 
 static void __exit udpCounter_exit(void)
 {
 	nf_unregister_hook(&nfho);           /*Unregister netfilter hook structure */
-	pr_debug("udp_packet_counter unloaded\n");
+	printk(KERN_INFO "udp_packet_counter unloaded\n");
 }
 
 module_init(udpCounter_init);
 module_exit(udpCounter_exit);
-
 
 MODULE_AUTHOR("shyam saini");
 MODULE_DESCRIPTION("Module for counting  UDP packets");
